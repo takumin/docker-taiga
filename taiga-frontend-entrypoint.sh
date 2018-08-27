53f69dfe385cb08c1cced57aa59b6fa14cbfa9a1
takumin <takumiiinn@gmail.com>
2018-08-27 18:26:52 +0900

    fix

--- taiga-frontend-entrypoint.sh
+++ taiga-frontend-entrypoint.sh
@@ -3,27 +3,27 @@
 set -eu
 
 if [ -n "${TAIGA_FRONTEND_EVENTS_BASE_URI}" ]; then
-  sed -i -e "s/TAIGA_FRONTEND_EVENTS_BASE_URI/${TAIGA_FRONTEND_EVENTS_BASE_URI}/" /taiga-front/conf.json
+  sed -i -e "s/TAIGA_FRONTEND_EVENTS_BASE_URI/${TAIGA_FRONTEND_EVENTS_BASE_URI}/" /taiga-frontend/conf.json
 else
   echo "Set require environment variable: TAIGA_FRONTEND_EVENTS_BASE_URI"
   exit 1
 fi
 
 if [ -n "${TAIGA_FRONTEND_BACKEND_BASE_URI}" ]; then
-  sed -i -e "s/TAIGA_FRONTEND_BACKEND_BASE_URI/${TAIGA_FRONTEND_BACKEND_BASE_URI}/" /taiga-front/conf.json
+  sed -i -e "s/TAIGA_FRONTEND_BACKEND_BASE_URI/${TAIGA_FRONTEND_BACKEND_BASE_URI}/" /taiga-frontend/conf.json
 else
   echo "Set require environment variable: TAIGA_FRONTEND_BACKEND_BASE_URI"
   exit 1
 fi
 
-echo "#!/bin/sh"                   >  /sv/taiga-front/run
-echo "set -e"                      >> /sv/taiga-front/run
-echo "exec 2>&1"                   >> /sv/taiga-front/run
-echo "exec nginx -g 'daemon off;'" >> /sv/taiga-front/run
-chmod 0755 /sv/taiga-front/run
+echo "#!/bin/sh"                   >  /sv/taiga-frontend/run
+echo "set -e"                      >> /sv/taiga-frontend/run
+echo "exec 2>&1"                   >> /sv/taiga-frontend/run
+echo "exec nginx -g 'daemon off;'" >> /sv/taiga-frontend/run
+chmod 0755 /sv/taiga-frontend/run
 
 if [ "$1" = 'default' ]; then
-  exec runsv taiga-front
+  exec runsv taiga-frontend
 fi
 
 exec "$@"
