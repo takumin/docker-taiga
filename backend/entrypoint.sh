222a2f98853d7d9dc5eea544a6c30d9659caef0e
takumin <takumiiinn@gmail.com>
2018-08-28 20:04:53 +0900

    fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,47 @@
 #!/bin/sh
 
 if [ "$1" = 'default' ]; then
+  ##############################################################################
+  # Check
+  ##############################################################################
+
+  set -e
+
   if [ ! -f "settings/local.py" ]; then
     echo "Require settings/local.py"
     exit 1
+  fi
+
+  if [ "x${TAIGA_BACKEND_DEBUG}" = 'xTrue' ]; then
+    echo "TAIGA_BACKEND_POSTGRESQL_HOST: \"${TAIGA_BACKEND_POSTGRESQL_HOST}\""
+    echo "TAIGA_BACKEND_POSTGRESQL_PORT: \"${TAIGA_BACKEND_POSTGRESQL_PORT}\""
+    echo "TAIGA_BACKEND_POSTGRESQL_NAME: \"${TAIGA_BACKEND_POSTGRESQL_NAME}\""
+    echo "TAIGA_BACKEND_POSTGRESQL_USER: \"${TAIGA_BACKEND_POSTGRESQL_USER}\""
+    echo "TAIGA_BACKEND_POSTGRESQL_PASS: \"${TAIGA_BACKEND_POSTGRESQL_PASS}\""
+    echo "TAIGA_BACKEND_RABBITMQ_USER: \"${TAIGA_BACKEND_RABBITMQ_USER}\""
+    echo "TAIGA_BACKEND_RABBITMQ_PASS: \"${TAIGA_BACKEND_RABBITMQ_PASS}\""
+    echo "TAIGA_BACKEND_RABBITMQ_HOST: \"${TAIGA_BACKEND_RABBITMQ_HOST}\""
+    echo "TAIGA_BACKEND_RABBITMQ_PORT: \"${TAIGA_BACKEND_RABBITMQ_PORT}\""
+    echo "TAIGA_BACKEND_RABBITMQ_PATH: \"${TAIGA_BACKEND_RABBITMQ_PATH}\""
+    echo "TAIGA_BACKEND_SITES_API_SCHEME: \"${TAIGA_BACKEND_SITES_API_SCHEME}\""
+    echo "TAIGA_BACKEND_SITES_API_DOMAIN: \"${TAIGA_BACKEND_SITES_API_DOMAIN}\""
+    echo "TAIGA_BACKEND_SITES_API_PORT: \"${TAIGA_BACKEND_SITES_API_PORT}\""
+    echo "TAIGA_BACKEND_SITES_FRONT_SCHEME: \"${TAIGA_BACKEND_SITES_FRONT_SCHEME}\""
+    echo "TAIGA_BACKEND_SITES_FRONT_DOMAIN: \"${TAIGA_BACKEND_SITES_FRONT_DOMAIN}\""
+    echo "TAIGA_BACKEND_SITES_FRONT_PORT: \"${TAIGA_BACKEND_SITES_FRONT_PORT}\""
+    echo "TAIGA_BACKEND_MEDIA_URL: \"${TAIGA_BACKEND_MEDIA_URL}\""
+    echo "TAIGA_BACKEND_STATIC_URL: \"${TAIGA_BACKEND_STATIC_URL}\""
+    echo "TAIGA_BACKEND_SECRET_KEY: \"${TAIGA_BACKEND_SECRET_KEY}\""
+    echo "TAIGA_BACKEND_DEBUG: \"${TAIGA_BACKEND_DEBUG}\""
+    echo "TAIGA_BACKEND_CELERY_ENABLED: \"${TAIGA_BACKEND_CELERY_ENABLED}\""
+    echo "TAIGA_BACKEND_ADMIN_NAME: \"${TAIGA_BACKEND_ADMIN_NAME}\""
+    echo "TAIGA_BACKEND_ADMIN_EMAIL: \"${TAIGA_BACKEND_ADMIN_EMAIL}\""
   fi
 
   ##############################################################################
   # PostgreSQL
   ##############################################################################
 
-  echo "TAIGA_BACKEND_POSTGRESQL_HOST: \"${TAIGA_BACKEND_POSTGRESQL_HOST}\""
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_HOST}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_HOST'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_HOST}'^" settings/local.py
   else
@@ -18,7 +49,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_POSTGRESQL_PORT: \"${TAIGA_BACKEND_POSTGRESQL_PORT}\""
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_PORT}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_PORT'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_PORT}'^" settings/local.py
   else
@@ -26,7 +56,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_POSTGRESQL_NAME: \"${TAIGA_BACKEND_POSTGRESQL_NAME}\""
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_NAME}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_NAME'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_NAME}'^" settings/local.py
   else
@@ -34,7 +63,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_POSTGRESQL_USER: \"${TAIGA_BACKEND_POSTGRESQL_USER}\""
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_USER}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_USER'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_USER}'^" settings/local.py
   else
@@ -42,7 +70,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_POSTGRESQL_PASS: \"${TAIGA_BACKEND_POSTGRESQL_PASS}\""
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_PASS}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_PASS'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_PASS}'^" settings/local.py
   else
@@ -54,11 +81,6 @@ if [ "$1" = 'default' ]; then
   # RabbitMQ
   ##############################################################################
 
-  echo "TAIGA_BACKEND_RABBITMQ_USER: \"${TAIGA_BACKEND_RABBITMQ_USER}\""
-  echo "TAIGA_BACKEND_RABBITMQ_PASS: \"${TAIGA_BACKEND_RABBITMQ_PASS}\""
-  echo "TAIGA_BACKEND_RABBITMQ_HOST: \"${TAIGA_BACKEND_RABBITMQ_HOST}\""
-  echo "TAIGA_BACKEND_RABBITMQ_PORT: \"${TAIGA_BACKEND_RABBITMQ_PORT}\""
-  echo "TAIGA_BACKEND_RABBITMQ_PATH: \"${TAIGA_BACKEND_RABBITMQ_PATH}\""
   if [ -n "${TAIGA_BACKEND_RABBITMQ_USER}" -a -n "${TAIGA_BACKEND_RABBITMQ_PASS}" -a -n "${TAIGA_BACKEND_RABBITMQ_HOST}" -a -n "${TAIGA_BACKEND_RABBITMQ_PORT}" ]; then
     sed -i -e "s^# \(EVENTS_PUSH_BACKEND = 'taiga.events.backends.rabbitmq.EventsPushBackend'\)$^\1^" settings/local.py
     sed -i -e "s^# \(.*\) {'url': 'ampq//TAIGA_BACKEND_RABBITMQ_USER:TAIGA_BACKEND_RABBITMQ_PASS@TAIGA_BACKEND_RABBITMQ_HOST:TAIGA_BACKEND_RABBITMQ_PORT/TAIGA_BACKEND_RABBITMQ_PATH'}$^\1 {'url': 'ampq//${TAIGA_BACKEND_RABBITMQ_USER}:${TAIGA_BACKEND_RABBITMQ_PASS}@${TAIGA_BACKEND_RABBITMQ_HOST}:${TAIGA_BACKEND_RABBITMQ_PORT}/${TAIGA_BACKEND_RABBITMQ_PATH}'}^" settings/local.py
@@ -74,7 +96,6 @@ if [ "$1" = 'default' ]; then
   # Sites
   ##############################################################################
 
-  echo "TAIGA_BACKEND_SITES_API_SCHEME: \"${TAIGA_BACKEND_SITES_API_SCHEME}\""
   if [ -n "${TAIGA_BACKEND_SITES_API_SCHEME}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_API_SCHEME'$^\1 = '${TAIGA_BACKEND_SITES_API_SCHEME}'^" settings/local.py
   else
@@ -82,8 +103,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_SITES_API_DOMAIN: \"${TAIGA_BACKEND_SITES_API_DOMAIN}\""
-  echo "TAIGA_BACKEND_SITES_API_PORT: \"${TAIGA_BACKEND_SITES_API_PORT}\""
   if [ -n "${TAIGA_BACKEND_SITES_API_DOMAIN}" -a -n "${TAIGA_BACKEND_SITES_API_PORT}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_API_DOMAIN:TAIGA_BACKEND_SITES_API_PORT'$^\1 = '${TAIGA_BACKEND_SITES_API_DOMAIN}:${TAIGA_BACKEND_SITES_API_PORT}'^" settings/local.py
   else
@@ -92,7 +111,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_SITES_FRONT_SCHEME: \"${TAIGA_BACKEND_SITES_FRONT_SCHEME}\""
   if [ -n "${TAIGA_BACKEND_SITES_FRONT_SCHEME}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_FRONT_SCHEME'$^\1 = '${TAIGA_BACKEND_SITES_FRONT_SCHEME}'^" settings/local.py
   else
@@ -100,8 +118,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_SITES_FRONT_DOMAIN: \"${TAIGA_BACKEND_SITES_FRONT_DOMAIN}\""
-  echo "TAIGA_BACKEND_SITES_FRONT_PORT: \"${TAIGA_BACKEND_SITES_FRONT_PORT}\""
   if [ -n "${TAIGA_BACKEND_SITES_FRONT_DOMAIN}" -a -n "${TAIGA_BACKEND_SITES_FRONT_PORT}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_FRONT_DOMAIN:TAIGA_BACKEND_SITES_FRONT_PORT'$^\1 = '${TAIGA_BACKEND_SITES_FRONT_DOMAIN}:${TAIGA_BACKEND_SITES_FRONT_PORT}'^" settings/local.py
   else
@@ -114,7 +130,6 @@ if [ "$1" = 'default' ]; then
   # URL
   ##############################################################################
 
-  echo "TAIGA_BACKEND_MEDIA_URL: \"${TAIGA_BACKEND_MEDIA_URL}\""
   if [ -n "${TAIGA_BACKEND_MEDIA_URL}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_MEDIA_URL'$^\1 = '${TAIGA_BACKEND_MEDIA_URL}'^" settings/local.py
   else
@@ -122,7 +137,6 @@ if [ "$1" = 'default' ]; then
     exit 1
   fi
 
-  echo "TAIGA_BACKEND_STATIC_URL: \"${TAIGA_BACKEND_STATIC_URL}\""
   if [ -n "${TAIGA_BACKEND_STATIC_URL}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_STATIC_URL'$^\1 = '${TAIGA_BACKEND_STATIC_URL}'^" settings/local.py
   else
@@ -134,7 +148,6 @@ if [ "$1" = 'default' ]; then
   # Secret
   ##############################################################################
 
-  echo "TAIGA_BACKEND_SECRET_KEY: \"${TAIGA_BACKEND_SECRET_KEY}\""
   if [ -n "${TAIGA_BACKEND_SECRET_KEY}" ]; then
     sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SECRET_KEY'$^\1 = '${TAIGA_BACKEND_SECRET_KEY}'^" settings/local.py
   else
@@ -145,8 +158,8 @@ if [ "$1" = 'default' ]; then
   ##############################################################################
   # Debug
   ##############################################################################
+
   if [ "x${TAIGA_BACKEND_DEBUG}" = 'xTrue' -o "x${TAIGA_BACKEND_DEBUG}" = 'xFalse' ]; then
-    echo "TAIGA_BACKEND_DEBUG: \"${TAIGA_BACKEND_DEBUG}\""
     echo ""                                   >> settings/local.py
     echo "DEBUG = \"${TAIGA_BACKEND_DEBUG}\"" >> settings/local.py
   fi
@@ -156,7 +169,6 @@ if [ "$1" = 'default' ]; then
   ##############################################################################
 
   if [ "x${TAIGA_BACKEND_CELERY_ENABLED}" = 'xTrue' -o "x${TAIGA_BACKEND_CELERY_ENABLED}" = 'xFalse' ]; then
-    echo "TAIGA_BACKEND_CELERY_ENABLED: \"${TAIGA_BACKEND_CELERY_ENABLED}\""
     echo ""                                                     >> settings/local.py
     echo "CELERY_ENABLED = \"${TAIGA_BACKEND_CELERY_ENABLED}\"" >> settings/local.py
   fi
@@ -165,8 +177,6 @@ if [ "$1" = 'default' ]; then
   # Admin
   ##############################################################################
   if [ -n "${TAIGA_BACKEND_ADMIN_NAME}" -a -n "${TAIGA_BACKEND_ADMIN_EMAIL}" ]; then
-    echo "TAIGA_BACKEND_ADMIN_NAME: \"${TAIGA_BACKEND_ADMIN_NAME}\""
-    echo "TAIGA_BACKEND_ADMIN_EMAIL: \"${TAIGA_BACKEND_ADMIN_EMAIL}\""
     echo ""                                                                     >> settings/local.py
     echo "ADMINS = ("                                                           >> settings/local.py
     echo "    ('${TAIGA_BACKEND_ADMIN_NAME}', '${TAIGA_BACKEND_ADMIN_EMAIL}')," >> settings/local.py
@@ -193,23 +203,27 @@ if [ "$1" = 'default' ]; then
   # Daemon
   ##############################################################################
 
-  echo "[watcher:taiga]" > circusd.ini
-  echo "working_dir = $(pwd)" >> circusd.ini
-  echo "cmd = gunicorn" >> circusd.ini
-  echo "args = -w 3 -t 60 --pythonpath=. -b ${TAIGA_BACKEND_SITES_FRONT_DOMAIN}:${TAIGA_BACKEND_SITES_FRONT_PORT} taiga.wsgi" >> circusd.ini
-  echo "numprocesses = 1" >> circusd.ini
-  echo "autostart = true" >> circusd.ini
-  echo "send_hup = true" >> circusd.ini
-  echo "" >> circusd.ini
-  echo "[env:taiga]" >> circusd.ini
-  echo "PATH = ${PATH}" >> circusd.ini
+  echo "[watcher:taiga]"                                                                        >  circusd.ini
+  echo "working_dir = $(pwd)"                                                                   >> circusd.ini
+  echo "cmd = gunicorn"                                                                         >> circusd.ini
+  echo "args = -w 3 -t 60 --pythonpath=. -b 0.0.0.0:${TAIGA_BACKEND_SITES_API_PORT} taiga.wsgi" >> circusd.ini
+  echo "numprocesses = 1"                                                                       >> circusd.ini
+  echo "autostart = true"                                                                       >> circusd.ini
+  echo "send_hup = true"                                                                        >> circusd.ini
+  echo ""                                                                                       >> circusd.ini
+  echo "[env:taiga]"                                                                            >> circusd.ini
+  echo "PATH = ${PATH}"                                                                         >> circusd.ini
 
   ##############################################################################
   # Running
   ##############################################################################
 
   echo "Starting Server"
-  exec circusd circusd.ini
+  if [ "x${TAIGA_BACKEND_DEBUG}" = 'xTrue' ]; then
+    exec python3 manage.py runserver
+  else
+    exec circusd circusd.ini
+  fi
 fi
 
 exec "$@"
