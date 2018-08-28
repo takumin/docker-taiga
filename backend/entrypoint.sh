1b6a3e0db24c3031f82717457a37d2e71528eda8
takumin <takumiiinn@gmail.com>
2018-08-28 18:28:25 +0900

    fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ "$1" = 'default' ]; then
-  if [ ! -f "local.py" ]; then
-    echo "Require local.py"
+  if [ ! -f "settings/local.py" ]; then
+    echo "Require settings/local.py"
     exit 1
   fi
 
@@ -11,7 +11,7 @@ if [ "$1" = 'default' ]; then
   ##############################################################################
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_HOST}" ]; then
     echo "TAIGA_BACKEND_POSTGRESQL_HOST: \"${TAIGA_BACKEND_POSTGRESQL_HOST}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_HOST'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_HOST}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_HOST'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_HOST}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_POSTGRESQL_HOST"
     exit 1
@@ -19,7 +19,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_PORT}" ]; then
     echo "TAIGA_BACKEND_POSTGRESQL_PORT: \"${TAIGA_BACKEND_POSTGRESQL_PORT}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_PORT'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_PORT}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_PORT'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_PORT}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_POSTGRESQL_PORT"
     exit 1
@@ -27,7 +27,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_NAME}" ]; then
     echo "TAIGA_BACKEND_POSTGRESQL_NAME: \"${TAIGA_BACKEND_POSTGRESQL_NAME}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_NAME'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_NAME}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_NAME'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_NAME}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_POSTGRESQL_NAME"
     exit 1
@@ -35,7 +35,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_USER}" ]; then
     echo "TAIGA_BACKEND_POSTGRESQL_USER: \"${TAIGA_BACKEND_POSTGRESQL_USER}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_USER'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_USER}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_USER'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_USER}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_POSTGRESQL_USER"
     exit 1
@@ -43,7 +43,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_POSTGRESQL_PASS}" ]; then
     echo "TAIGA_BACKEND_POSTGRESQL_PASS: \"${TAIGA_BACKEND_POSTGRESQL_PASS}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_PASS'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_PASS}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_POSTGRESQL_PASS'$^\1 = '${TAIGA_BACKEND_POSTGRESQL_PASS}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_POSTGRESQL_PASS"
     exit 1
@@ -59,7 +59,7 @@ if [ "$1" = 'default' ]; then
     echo "TAIGA_BACKEND_RABBITMQ_HOST: \"${TAIGA_BACKEND_RABBITMQ_HOST}\""
     echo "TAIGA_BACKEND_RABBITMQ_PORT: \"${TAIGA_BACKEND_RABBITMQ_PORT}\""
     echo "TAIGA_BACKEND_RABBITMQ_PATH: \"${TAIGA_BACKEND_RABBITMQ_PATH}\""
-    sed -i -e "s^# \(.*\) = 'ampq//TAIGA_BACKEND_RABBITMQ_USER:TAIGA_BACKEND_RABBITMQ_PASS@TAIGA_BACKEND_RABBITMQ_HOST:TAIGA_BACKEND_RABBITMQ_PORT/TAIGA_BACKEND_RABBITMQ_PATH'$^\1 = 'ampq//${TAIGA_BACKEND_RABBITMQ_USER}:${TAIGA_BACKEND_RABBITMQ_PASS}@${TAIGA_BACKEND_RABBITMQ_HOST}:${TAIGA_BACKEND_RABBITMQ_PORT}/${TAIGA_BACKEND_RABBITMQ_PATH}'^" local.py
+    sed -i -e "s^# \(.*\) = 'ampq//TAIGA_BACKEND_RABBITMQ_USER:TAIGA_BACKEND_RABBITMQ_PASS@TAIGA_BACKEND_RABBITMQ_HOST:TAIGA_BACKEND_RABBITMQ_PORT/TAIGA_BACKEND_RABBITMQ_PATH'$^\1 = 'ampq//${TAIGA_BACKEND_RABBITMQ_USER}:${TAIGA_BACKEND_RABBITMQ_PASS}@${TAIGA_BACKEND_RABBITMQ_HOST}:${TAIGA_BACKEND_RABBITMQ_PORT}/${TAIGA_BACKEND_RABBITMQ_PATH}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_RABBITMQ_USER,"
     echo "                                  TAIGA_BACKEND_RABBITMQ_PASS,"
@@ -75,7 +75,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_SITES_API_SCHEME}" ]; then
     echo "TAIGA_BACKEND_SITES_API_SCHEME: \"${TAIGA_BACKEND_SITES_API_SCHEME}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_API_SCHEME'$^\1 = '${TAIGA_BACKEND_SITES_API_SCHEME}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_API_SCHEME'$^\1 = '${TAIGA_BACKEND_SITES_API_SCHEME}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_SITES_API_SCHEME"
     exit 1
@@ -84,7 +84,7 @@ if [ "$1" = 'default' ]; then
   if [ -n "${TAIGA_BACKEND_SITES_API_DOMAIN}" -a -n "${TAIGA_BACKEND_SITES_API_PORT}" ]; then
     echo "TAIGA_BACKEND_SITES_API_DOMAIN: \"${TAIGA_BACKEND_SITES_API_DOMAIN}\""
     echo "TAIGA_BACKEND_SITES_API_PORT: \"${TAIGA_BACKEND_SITES_API_PORT}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_API_DOMAIN:TAIGA_BACKEND_SITES_API_PORT'$^\1 = '${TAIGA_BACKEND_SITES_API_DOMAIN}:${TAIGA_BACKEND_SITES_API_PORT}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_API_DOMAIN:TAIGA_BACKEND_SITES_API_PORT'$^\1 = '${TAIGA_BACKEND_SITES_API_DOMAIN}:${TAIGA_BACKEND_SITES_API_PORT}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_SITES_API_DOMAIN,"
     echo "                                  TAIGA_BACKEND_SITES_API_PORT"
@@ -93,7 +93,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_SITES_FRONT_SCHEME}" ]; then
     echo "TAIGA_BACKEND_SITES_FRONT_SCHEME: \"${TAIGA_BACKEND_SITES_FRONT_SCHEME}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_FRONT_SCHEME'$^\1 = '${TAIGA_BACKEND_SITES_FRONT_SCHEME}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_FRONT_SCHEME'$^\1 = '${TAIGA_BACKEND_SITES_FRONT_SCHEME}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_SITES_FRONT_SCHEME"
     exit 1
@@ -102,7 +102,7 @@ if [ "$1" = 'default' ]; then
   if [ -n "${TAIGA_BACKEND_SITES_FRONT_DOMAIN}" -a -n "${TAIGA_BACKEND_SITES_FRONT_PORT}" ]; then
     echo "TAIGA_BACKEND_SITES_FRONT_DOMAIN: \"${TAIGA_BACKEND_SITES_FRONT_DOMAIN}\""
     echo "TAIGA_BACKEND_SITES_FRONT_PORT: \"${TAIGA_BACKEND_SITES_FRONT_PORT}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_FRONT_DOMAIN:TAIGA_BACKEND_SITES_FRONT_PORT'$^\1 = '${TAIGA_BACKEND_SITES_FRONT_DOMAIN}:${TAIGA_BACKEND_SITES_FRONT_PORT}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SITES_FRONT_DOMAIN:TAIGA_BACKEND_SITES_FRONT_PORT'$^\1 = '${TAIGA_BACKEND_SITES_FRONT_DOMAIN}:${TAIGA_BACKEND_SITES_FRONT_PORT}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_SITES_FRONT_DOMAIN,"
     echo "                                  TAIGA_BACKEND_SITES_FRONT_PORT"
@@ -115,7 +115,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_MEDIA_URL}" ]; then
     echo "TAIGA_BACKEND_MEDIA_URL: \"${TAIGA_BACKEND_MEDIA_URL}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_MEDIA_URL'$^\1 = '${TAIGA_BACKEND_MEDIA_URL}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_MEDIA_URL'$^\1 = '${TAIGA_BACKEND_MEDIA_URL}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_MEDIA_URL"
     exit 1
@@ -123,7 +123,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_STATIC_URL}" ]; then
     echo "TAIGA_BACKEND_STATIC_URL: \"${TAIGA_BACKEND_STATIC_URL}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_STATIC_URL'$^\1 = '${TAIGA_BACKEND_STATIC_URL}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_STATIC_URL'$^\1 = '${TAIGA_BACKEND_STATIC_URL}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_STATIC_URL"
     exit 1
@@ -135,7 +135,7 @@ if [ "$1" = 'default' ]; then
 
   if [ -n "${TAIGA_BACKEND_SECRET_KEY}" ]; then
     echo "TAIGA_BACKEND_SECRET_KEY: \"${TAIGA_BACKEND_SECRET_KEY}\""
-    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SECRET_KEY'$^\1 = '${TAIGA_BACKEND_SECRET_KEY}'^" local.py
+    sed -i -e "s^# \(.*\) = 'TAIGA_BACKEND_SECRET_KEY'$^\1 = '${TAIGA_BACKEND_SECRET_KEY}'^" settings/local.py
   else
     echo "Set require environment variable: TAIGA_BACKEND_SECRET_KEY"
     exit 1
@@ -146,8 +146,8 @@ if [ "$1" = 'default' ]; then
   ##############################################################################
   if [ "x${TAIGA_BACKEND_DEBUG}" = 'xTrue' -o "x${TAIGA_BACKEND_DEBUG}" = 'xFalse' ]; then
     echo "TAIGA_BACKEND_DEBUG: \"${TAIGA_BACKEND_DEBUG}\""
-    echo ""                                   >> local.py
-    echo "DEBUG = \"${TAIGA_BACKEND_DEBUG}\"" >> local.py
+    echo ""                                   >> settings/local.py
+    echo "DEBUG = \"${TAIGA_BACKEND_DEBUG}\"" >> settings/local.py
   fi
 
   ##############################################################################
@@ -156,8 +156,8 @@ if [ "$1" = 'default' ]; then
 
   if [ "x${TAIGA_BACKEND_CELERY_ENABLED}" = 'xTrue' -o "x${TAIGA_BACKEND_CELERY_ENABLED}" = 'xFalse' ]; then
     echo "TAIGA_BACKEND_CELERY_ENABLED: \"${TAIGA_BACKEND_CELERY_ENABLED}\""
-    echo ""                                                     >> local.py
-    echo "CELERY_ENABLED = \"${TAIGA_BACKEND_CELERY_ENABLED}\"" >> local.py
+    echo ""                                                     >> settings/local.py
+    echo "CELERY_ENABLED = \"${TAIGA_BACKEND_CELERY_ENABLED}\"" >> settings/local.py
   fi
 
   ##############################################################################
@@ -166,10 +166,10 @@ if [ "$1" = 'default' ]; then
   if [ -n "${TAIGA_BACKEND_ADMIN_NAME}" -a -n "${TAIGA_BACKEND_ADMIN_EMAIL}" ]; then
     echo "TAIGA_BACKEND_ADMIN_NAME: \"${TAIGA_BACKEND_ADMIN_NAME}\""
     echo "TAIGA_BACKEND_ADMIN_EMAIL: \"${TAIGA_BACKEND_ADMIN_EMAIL}\""
-    echo ""                                                                     >> local.py
-    echo "ADMINS = ("                                                           >> local.py
-    echo "    ('${TAIGA_BACKEND_ADMIN_NAME}', '${TAIGA_BACKEND_ADMIN_EMAIL}')," >> local.py
-    echo ")"                                                                    >> local.py
+    echo ""                                                                     >> settings/local.py
+    echo "ADMINS = ("                                                           >> settings/local.py
+    echo "    ('${TAIGA_BACKEND_ADMIN_NAME}', '${TAIGA_BACKEND_ADMIN_EMAIL}')," >> settings/local.py
+    echo ")"                                                                    >> settings/local.py
   fi
 
   ##############################################################################
