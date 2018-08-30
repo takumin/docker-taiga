9ed88f4b8124409fc2224890e1937b0412fb278f
takumin <takumiiinn@gmail.com>
2018-08-31 00:37:04 +0900

    fix

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -7,8 +7,8 @@ if [ "$1" = 'default' ]; then
   # Wait
   ##############################################################################
 
-  dockerize -wait tcp://${BACKEND_LISTEN_HOST} -timeout 30s
-  dockerize -wait tcp://${EVENTS_LISTEN_HOST} -timeout 30s
+  dockerize -wait tcp://${EVENTS_LISTEN_HOST} -timeout 10s
+  dockerize -wait tcp://${BACKEND_LISTEN_HOST} -timeout 60s
 
   ##############################################################################
   # Service Initialized
