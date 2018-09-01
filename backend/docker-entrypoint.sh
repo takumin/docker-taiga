5f9e777bd7abc03d7f2696e987116523d8f005a0
takumin <takumiiinn@gmail.com>
2018-09-01 23:56:58 +0900

    fix

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -70,70 +70,95 @@ if [ "$1" = 'default' ]; then
   # Daemon Initialize
   ##############################################################################
 
-  echo "" > taiga.ini
+  # mkdir -p gunicorn
+  # echo '#!/bin/sh'                                                                              >  gunicorn/run
+  # echo 'cd /taiga-backend'                                                                      >> gunicorn/run
+  # echo 'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'               >> gunicorn/run
+  # echo "export PYTHONPATH=.:/usr/local/lib/python${PYTHON_MEJOR}.${PYTHON_MINOR}/site-packages" >> gunicorn/run
+  # echo 'exec 2>&1'                                                                              >> gunicorn/run
+  # echo 'exec gunicorn -w GUNICORN_WORKER -t GUNICORN_TIMEOUT -b 0.0.0.0:8080 taiga.wsgi'        >> gunicorn/run
+  # chmod 0755 gunicorn/run
 
-  echo "[watcher:taiga]"                                                                 >> taiga.ini
-  echo "working_dir = /taiga-backend"                                                    >> taiga.ini
-  echo "cmd = gunicorn"                                                                  >> taiga.ini
-  echo "args = -w GUNICORN_WORKER -t GUNICORN_TIMEOUT -b 0.0.0.0:8080 taiga.wsgi"        >> taiga.ini
-  echo "numprocesses = 1"                                                                >> taiga.ini
-  echo "autostart = true"                                                                >> taiga.ini
-  echo "send_hup = true"                                                                 >> taiga.ini
-  echo ""                                                                                >> taiga.ini
-  echo "[env:taiga]"                                                                     >> taiga.ini
-  echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"               >> taiga.ini
-  echo "PYTHONPATH=.:/usr/local/lib/python${PYTHON_MEJOR}.${PYTHON_MINOR}/site-packages" >> taiga.ini
+  # echo "" > taiga.ini
+  #
+  # echo "[watcher:taiga]"                                                                 >> taiga.ini
+  # echo "working_dir = /taiga-backend"                                                    >> taiga.ini
+  # echo "cmd = gunicorn"                                                                  >> taiga.ini
+  # echo "args = -w GUNICORN_WORKER -t GUNICORN_TIMEOUT -b 0.0.0.0:8080 taiga.wsgi"        >> taiga.ini
+  # echo "numprocesses = 1"                                                                >> taiga.ini
+  # echo "autostart = true"                                                                >> taiga.ini
+  # echo "send_hup = true"                                                                 >> taiga.ini
+  # echo ""                                                                                >> taiga.ini
+  # echo "[env:taiga]"                                                                     >> taiga.ini
+  # echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"               >> taiga.ini
+  # echo "PYTHONPATH=.:/usr/local/lib/python${PYTHON_MEJOR}.${PYTHON_MINOR}/site-packages" >> taiga.ini
 
-  if grep -qs '^CELERY_ENABLED = True$' settings/local.py; then
-    echo "[watcher:taiga-celery]"                                                          >> taiga.ini
-    echo "working_dir = /taiga-backend"                                                    >> taiga.ini
-    echo "cmd = celery"                                                                    >> taiga.ini
-    echo "args = -A taiga worker -c CELERY_WORKER --time-limit CELERY_TIMEOUT"             >> taiga.ini
-    echo "numprocesses = 1"                                                                >> taiga.ini
-    echo "autostart = true"                                                                >> taiga.ini
-    echo "send_hup = true"                                                                 >> taiga.ini
-    echo ""                                                                                >> taiga.ini
-    echo "[env:taiga-celery]"                                                              >> taiga.ini
-    echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"               >> taiga.ini
-    echo "PYTHONPATH=.:/usr/local/lib/python${PYTHON_MEJOR}.${PYTHON_MINOR}/site-packages" >> taiga.ini
-  fi
+  # if grep -qs '^CELERY_ENABLED = True$' settings/local.py; then
+  #   echo "[watcher:taiga-celery]"                                                          >> taiga.ini
+  #   echo "working_dir = /taiga-backend"                                                    >> taiga.ini
+  #   echo "cmd = celery"                                                                    >> taiga.ini
+  #   echo "args = -A taiga worker -c CELERY_WORKER --time-limit CELERY_TIMEOUT"             >> taiga.ini
+  #   echo "numprocesses = 1"                                                                >> taiga.ini
+  #   echo "autostart = true"                                                                >> taiga.ini
+  #   echo "send_hup = true"                                                                 >> taiga.ini
+  #   echo ""                                                                                >> taiga.ini
+  #   echo "[env:taiga-celery]"                                                              >> taiga.ini
+  #   echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"               >> taiga.ini
+  #   echo "PYTHONPATH=.:/usr/local/lib/python${PYTHON_MEJOR}.${PYTHON_MINOR}/site-packages" >> taiga.ini
+  # fi
 
   ##############################################################################
   # Daemon Parameter
   ##############################################################################
 
-  if [ -n "${BACKEND_GUNICORN_WORKER}" ]; then
-    sed -i -e "s/GUNICORN_WORKER/${BACKEND_GUNICORN_WORKER}/" taiga.ini
-  else
-    sed -i -e "s/GUNICORN_WORKER/1/" taiga.ini
-  fi
+  # if [ -n "${BACKEND_GUNICORN_WORKER}" ]; then
+  #   sed -i -e "s/GUNICORN_WORKER/${BACKEND_GUNICORN_WORKER}/" gunicorn/run
+  # else
+  #   sed -i -e "s/GUNICORN_WORKER/1/" gunicorn/run
+  # fi
 
-  if [ -n "${BACKEND_GUNICORN_TIMEOUT}" ]; then
-    sed -i -e "s/GUNICORN_TIMEOUT/${BACKEND_GUNICORN_TIMEOUT}/" taiga.ini
-  else
-    sed -i -e "s/GUNICORN_TIMEOUT/3/" taiga.ini
-  fi
+  # if [ -n "${BACKEND_GUNICORN_TIMEOUT}" ]; then
+  #   sed -i -e "s/GUNICORN_TIMEOUT/${BACKEND_GUNICORN_TIMEOUT}/" gunicorn/run
+  # else
+  #   sed -i -e "s/GUNICORN_TIMEOUT/3/" gunicorn/run
+  # fi
 
-  if grep -qs '^CELERY_ENABLED = True$' settings/local.py; then
-    if [ -n "${BACKEND_CELERY_WORKER}" ]; then
-      sed -i -e "s/CELERY_WORKER/${BACKEND_CELERY_WORKER}/" taiga.ini
-    else
-      sed -i -e 's/CELERY_WORKER/1/' taiga.ini
-    fi
+  # if [ -n "${BACKEND_GUNICORN_WORKER}" ]; then
+  #   sed -i -e "s/GUNICORN_WORKER/${BACKEND_GUNICORN_WORKER}/" taiga.ini
+  # else
+  #   sed -i -e "s/GUNICORN_WORKER/1/" taiga.ini
+  # fi
+  #
+  # if [ -n "${BACKEND_GUNICORN_TIMEOUT}" ]; then
+  #   sed -i -e "s/GUNICORN_TIMEOUT/${BACKEND_GUNICORN_TIMEOUT}/" taiga.ini
+  # else
+  #   sed -i -e "s/GUNICORN_TIMEOUT/3/" taiga.ini
+  # fi
 
-    if [ -n "${BACKEND_CELERY_TIMEOUT}" ]; then
-      sed -i -e "s/CELERY_TIMEOUT/${BACKEND_CELERY_TIMEOUT}/" taiga.ini
-    else
-      sed -i -e 's/CELERY_TIMEOUT/3/' taiga.ini
-    fi
-  fi
+  # if grep -qs '^CELERY_ENABLED = True$' settings/local.py; then
+  #   if [ -n "${BACKEND_CELERY_WORKER}" ]; then
+  #     sed -i -e "s/CELERY_WORKER/${BACKEND_CELERY_WORKER}/" taiga.ini
+  #   else
+  #     sed -i -e 's/CELERY_WORKER/1/' taiga.ini
+  #   fi
+  #
+  #   if [ -n "${BACKEND_CELERY_TIMEOUT}" ]; then
+  #     sed -i -e "s/CELERY_TIMEOUT/${BACKEND_CELERY_TIMEOUT}/" taiga.ini
+  #   else
+  #     sed -i -e 's/CELERY_TIMEOUT/3/' taiga.ini
+  #   fi
+  # fi
 
   ##############################################################################
   # Daemon Running
   ##############################################################################
 
   echo "Starting Server"
-  exec circusd taiga.ini
+  exec gunicorn -w ${BACKEND_GUNICORN_WORKER} -t ${BACKEND_GUNICORN_TIMEOUT} -b 0.0.0.0:8080 taiga.wsgi
+  # mkdir -p service
+  # ln -s ../gunicorn service/gunicorn
+  # exec runsvdir service
+  # exec circusd taiga.ini
 fi
 
 exec "$@"
